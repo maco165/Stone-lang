0893ad18097ac7641b873f157632df791e644874
maco <mtgoxtvmax7@gmail.com>
2022-08-13 20:20:12 +0900

    構文解析　ライブラリとテスト(仮)

--- stone/src/BasicParser.java
+++ stone/src/BasicParser.java
@@ -8,25 +8,25 @@ public class BasicParser {
     HashSet<String> reserved = new Hashset<String>();
     Operators operators = new Operators();
     Parser expr0 = rule();
-    Parse primary = rule(PrimaryExpr.class)
+    Parser primary = rule(PrimaryExpr.class)
         .or(rule().sep("(")).ast(expr0).sep(")"),
             rule().number(NumberLiteral.class),
             rule().identifier(Name.class, reserved),
             rule().string(StringLiteral.class));
-    Parse factor = rule().or(rule(NegativeExpr.class).sep("-").ast(primary),primary);
-    Parse expr = expr0.expression(BinaryExpr.class, factor, operators);
+    Parser factor = rule().or(rule(NegativeExpr.class).sep("-").ast(primary),primary);
+    Parser expr = expr0.expression(BinaryExpr.class, factor, operators);
 
-    Parse statement0 = rule();
-    Parse block = rule(BlockStmnt.class)
+    Parser statement0 = rule();
+    Parser block = rule(BlockStmnt.class)
         .sep("{").option(statement())
         .repeat(rule().sep(";", Token.EOL).option(statement0))
         .sep("}");
-    Parse simple = rule(PrimaryExpr.class).ast(expr);
-    Parse statement = statement0.or(
+    Parser simple = rule(PrimaryExpr.class).ast(expr);
+    Parser statement = statement0.or(
         rule(Ifstmnt.class).sep("if").ast(expr).ast(block).option(rule().sep("else").ast(block)),
         rule(WhileStmnt.class).sep("while").ast(expr).ast(block), simple);
     
-    Parse program = rule().or(statement, rule(NullStmnt.class)).sep(";", Token.EOL);
+    Parser program = rule().or(statement, rule(NullStmnt.class)).sep(";", Token.EOL);
 
     public BasicParser() {
         reserved.add(";");
---
+++ stone/src/test/ParserRunner.java
@@ -0,0 +1,14 @@
+package stone.src.test;
+import stone.src.ast.ASTree;
+import stone.src.*;
+
+public class ParserRunner {
+    public static void main(String[] args) throws ParseException {
+        Lexer l = new Lexer(new CodeDialog());
+        BasicParser bp = new BasicParser();
+        while (l.peek(0) != Token.EOF) {
+            ASTree ast = bp.parse(l);
+            System.out.println("=> " + ast.toString());
+        }
+    }
+}
